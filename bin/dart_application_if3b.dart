4f23c990c73ba1e56f915c56e7235cebabcabbc1
andreasMhs <andreas14477@mhs.mdp.ac.id>
2023-10-03 12:22:16 +0700

    Initial Project

---
+++ bin/dart_application_if3b.dart
@@ -0,0 +1,11 @@
+import 'package:dart_application_if3b/dart_application_if3b.dart'
+    as dart_application_if3b;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_if3b.calculate()}!');
+}
+
+//cek git config global ==> git config --global --list
+//(cara hapus git config global) 
+//git config --global --unset user.name
+//git config --global --unset user.email
