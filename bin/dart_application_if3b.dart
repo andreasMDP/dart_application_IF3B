adec73f59a9a2564e696d2c0bf6ef2dca8d64dca
andreasMhs <andreas14477@mhs.mdp.ac.id>
2023-10-03 12:45:08 +0700

    Change to hello everyone

--- bin/dart_application_if3b.dart
+++ bin/dart_application_if3b.dart
@@ -2,7 +2,7 @@ import 'package:dart_application_if3b/dart_application_if3b.dart'
     as dart_application_if3b;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_if3b.calculate()}!');
+  print('Hello everyone');
 }
 
 //cek git config global ==> git config --global --list
