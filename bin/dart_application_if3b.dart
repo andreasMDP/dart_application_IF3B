86a5624ba6c4a76f81c4ddbc39bf79508f6ab7e5
andreasMhs <andreas14477@mhs.mdp.ac.id>
2023-10-03 12:38:47 +0700

    Updated project

--- bin/dart_application_if3b.dart
+++ bin/dart_application_if3b.dart
@@ -9,3 +9,11 @@ void main(List<String> arguments) {
 //(cara hapus git config global) 
 //git config --global --unset user.name
 //git config --global --unset user.email
+
+//git init 
+//git add .
+//git commit -m "Initial Project" (-M artinya message)
+//git branch --show-current (untuk lihat branch yang sekarang -> default: master)
+//git branch -M main
+//git remote add origin https:....
+//git push -u origin main
